2bf894c97266618c0783cad6027c9b55e81f5b31
eugeny <eberkunsky@gmail.com>
2018-06-01 21:32:51 +0300

    2-4-6 pre

---
+++ src/lab24/Task246.java
@@ -0,0 +1,10 @@
+package lab24;
+
+public class Task246 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%"+(n-i)+"d\n",1);
+        }
+    }
+}
