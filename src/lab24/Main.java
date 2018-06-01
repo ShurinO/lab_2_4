e24b93180bf5fdf613c8e5c5f39e48099b503eed
eugeny <eberkunsky@gmail.com>
2018-06-01 20:51:11 +0300

    2-4-3

--- src/lab24/Main.java
+++ src/lab24/Main.java
@@ -1,6 +1,7 @@
 package lab24;
 
 import task.Calculation;
+import task.MyMath;
 
 public class Main {
 
@@ -8,5 +9,7 @@ public class Main {
         Calculation.calc(); // обращаемся к статическому методу calc() класса Calculation
         Calculation calculation = new Calculation(); // создаем объект
         calculation.calc(); // неправильно обращаемся к статическому методу класса, как к обычному методу
+
+        System.out.println("MyMath.areaOfCircle(10) = " + MyMath.areaOfCircle(10));
     }
 }
--- src/task/MyMath.java
+++ src/task/MyMath.java
@@ -1,6 +1,13 @@
 package task;
 
 public class MyMath {
+
+    final static double PI = 3.14;
+
+    public static double areaOfCircle(double radius) {
+        return PI*radius*radius;
+    }
+
     public static int findMin(int[] arr) {
         int res = arr[0];
         for (int i = 1; i < arr.length; i++) {
